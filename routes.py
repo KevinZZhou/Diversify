2f8b06cd1c17a0ad3452979ca72120fb69b7aacf
Kevin Zhou <53985119+KevinZZhou@users.noreply.github.com>
2021-08-15 23:23:17 -0400

    Create callback() in routes.py
    Added a function to obtain the access and refresh tokens if authorization is granted, redirects to landing page otherwise

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 # Imports
 from __main__ import app
-from flask import render_template, redirect, abort, session
+from flask import render_template, redirect, abort, request, session
 from flask.helpers import make_response
+import requests
 from secrets import choice
 from string import ascii_letters, digits
 from urllib.parse import urlencode
@@ -66,3 +67,32 @@ def login():
 @app.route('/account', methods = ['POST', 'GET'])
 def account():
     return redirect('https://accounts.spotify.com/en/logout')
+
+# Redirects to different pages depending on the result of Spotify authorization
+@app.route('/callback', methods = ['POST', 'GET'])
+def callback():
+    # For rejected authorization, add session['alert'], redirect to landing page
+    if 'error' in request.args:
+        session['alert'] = True
+        return redirect('/')
+    
+    # Request access and refresh tokens
+    data = {
+        'grant_type': 'authorization_code',
+        'code': request.args.get('code'),
+        'redirect_uri': REDIRECT_URI,
+    }
+    response = requests.post(TOKEN_URL, 
+        auth = (CLIENT_ID, CLIENT_SECRET), 
+        data = data
+    )
+    response_data = response.json()
+
+    # If the request is not successful, abort
+    if response.status_code != 200:
+        abort(response.status_code)
+    
+    # Add the tokens to session, redirect to generate()
+    session['access_token'] = response_data.get('access_token')
+    session['refresh_token'] = response_data.get('refresh_token')
+    return redirect('/generate')
