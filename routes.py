782176f882fe167d13a10c8edac8f5fa9f882c25
Kevin Zhou <53985119+KevinZZhou@users.noreply.github.com>
2021-08-15 23:05:47 -0400

    Create login() in routes.py
    Added a function to handle Spotify account authorization

--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
 # Imports
 from __main__ import app
 from flask import render_template, redirect, abort, session
+from flask.helpers import make_response
+from secrets import choice
+from string import ascii_letters, digits
+from urllib.parse import urlencode
 from config import *
 from forms import *
 
@@ -22,17 +26,38 @@ def landing():
         countries = country_ids.keys(),
     )
 
-# Page displaying the Top 50 for a particular country
+# Page displaying the Top 50 playlist for a particular country
 @app.route('/country=<place>', methods = ['POST', 'GET'])
 def country(place):
+    # Create "forms" (i.e., buttons)
     generate: FlaskForm = GeneratePlaylistForm()
     account: FlaskForm = AccountForm()
+
+    # Return the country page if the country input is valid, 404 error otherwise
     try: 
         return render_template('country.html', 
             generate = generate, 
             account = account, 
             country = place, 
-            playlist_link = country_ids[place]
+            country_embed = EMBED_URL + country_ids[place]
         )
     except KeyError:
         abort(404)
+
+# Handles Spotify account authorization
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    # Query to be sent for authorization
+    data = {
+        'client_id': CLIENT_ID, 
+        'response_type': 'code', 
+        'redirect_uri': REDIRECT_URI, 
+        'state': ''.join(choice(ascii_letters + digits) for _ in range(16)), 
+        'scope': 'user-read-private user-read-email', 
+        'show_dialog': True
+    }
+    
+    # Send the authorization request to Spotify
+    response = make_response(redirect(f'{AUTH_URL}/?{urlencode(data)}'))
+    response.set_cookie('spotify_auth_state', data['state'])
+    return response
