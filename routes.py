09a78db42ef90e55fb2b72eb76d39fa538b483b0
Kevin Zhou <53985119+KevinZZhou@users.noreply.github.com>
2021-08-15 22:12:35 -0400

    Create country() in routes.py
    Created a function for the country page, which uses the country input from the landing page

--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
 # Imports
 from __main__ import app
-from flask import render_template, redirect, session
+from flask import render_template, redirect, abort, session
 from config import *
 from forms import *
 
 
 # Routes
+# Landing page with country input
 @app.route('/', methods = ['POST', 'GET'])
 def landing():
     # Create form for country input
@@ -16,6 +17,22 @@ def landing():
         return redirect('/country=' + form.country.data)
     
     # If not submitted, return the landing page with the form
-    return render_template('landing.html', form = form, 
-        countries = country_ids.keys()
+    return render_template('landing.html', 
+        form = form, 
+        countries = country_ids.keys(),
     )
+
+# Page displaying the Top 50 for a particular country
+@app.route('/country=<place>', methods = ['POST', 'GET'])
+def country(place):
+    generate: FlaskForm = GeneratePlaylistForm()
+    account: FlaskForm = AccountForm()
+    try: 
+        return render_template('country.html', 
+            generate = generate, 
+            account = account, 
+            country = place, 
+            playlist_link = country_ids[place]
+        )
+    except KeyError:
+        abort(404)
