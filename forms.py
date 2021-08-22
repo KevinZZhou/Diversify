603f25d060a32a635b8fd2d654fb86b39838ed3b
Kevin Zhou <53985119+KevinZZhou@users.noreply.github.com>
2021-08-22 04:02:42 -0400

    Change config variables in forms.py
    Updated forms.py to use Heroku config variables

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
 from wtforms.validators import InputRequired
-from config import country_ids
+import os
 
 class CountryForm(FlaskForm):
     country = SelectField('Country', validators = [InputRequired()], 
-        choices = country_ids.keys())
+        choices = dict(os.environ['COUNTRY_IDS']).keys())
     submit = SubmitField('Submit')
 
 class GeneratePlaylistForm(FlaskForm):
